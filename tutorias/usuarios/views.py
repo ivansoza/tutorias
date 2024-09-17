d8c2d7ecb886cad1e57e48aeefb4c916fb0e95c2
ivansoza <ivansz123@gmail.com>
2024-09-16 22:36:51 -0600

    "Updated base.html template, added new template for docentes_list.html, modified views.py to filter docentes by posgrado and added context data for template"

--- tutorias/usuarios/views.py
+++ tutorias/usuarios/views.py
@@ -34,12 +34,49 @@ class DocentesListView(ListView):
     context_object_name = 'docentes'
 
     def get_queryset(self):
+        # Filtrar por grupo 'Docente'
         docentes_group = Group.objects.get(name='Docente')
-        return CustomUser.objects.filter(groups=docentes_group)
+        queryset = CustomUser.objects.filter(groups=docentes_group)
+
+        # Filtrar por posgrado_docente si se proporciona el ID del posgrado a través del GET request
+        posgrado_id = self.request.GET.get('posgrado_id')
+        if posgrado_id:
+            queryset = queryset.filter(posgrado_docente__id=posgrado_id)
+
+        return queryset
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
+        # Obtiene la queryset filtrada
+        filtered_queryset = self.get_queryset()
+
+        # Calcula el total de docentes activos en la queryset filtrada
+        context['total_docentes'] = filtered_queryset.filter(is_active=True).count()
+
+        # Calcula el total de maestrías y doctorados entre los docentes en la queryset filtrada
+        context['total_maestria'] = filtered_queryset.filter(
+            posgrado_docente__nombre__in=[
+                "Maestría en Sistemas Computacionales",
+                "Maestría en Ingeniería Mecatrónica",
+                "Maestría en Ingeniería Administrativa"
+            ]).count()
+
+        context['total_doctorado'] = filtered_queryset.filter(
+            posgrado_docente__nombre="Doctorado en Ciencias de la Ingeniería"
+        ).count()
+
+        # Añade los posgrados al contexto para el selector
+        context['posgrados'] = Posgrado.objects.all()
+
+        # Meta información de la página
+        context['dashboard_title'] = 'Lista de Docentes'
+        context['breadcrumb_active_item'] = 'Lista de Docente'
         context['navbar'] = 'docente'
+        context['url'] = 'home'
+
         return context
+
 
 class AlumnosListView(ListView):
     model = CustomUser
